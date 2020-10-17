36a8c049bc27457b2e747fe25424a11ae7360da5
vivencio2 <jayr.anes@gmail.com>
2020-10-18 01:21:13 +0800

    pdf generator

--- src/app/certificate/certificate.component.ts
+++ src/app/certificate/certificate.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EncryptionService } from '../app.service';
 import { AttendeeService } from '../attendee.service';
+import {jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'gdgph-certificate',
@@ -15,5 +17,16 @@ export class CertificateComponent {
     console.log()
     var attendee = this.attendeeService.filter(this.encryptionService.decrypt(decodeURIComponent(this.encTicketNumber)));
     this.attendeeName = attendee[0].fullName;
+  }
+  download() {
+    var element = document.getElementById('certificate');
+    html2canvas(element).then((canvas) => {
+      console.log(canvas);
+      var screen = canvas.toDataURL('image/png');
+      var doc = new jsPDF();
+      var height = canvas.height * 208 / canvas.width;
+      doc.addImage(screen, 0, 0, 208, height);
+      doc.save("DevFestPH2020Certificate.pdf");
+    });
   }
 }
